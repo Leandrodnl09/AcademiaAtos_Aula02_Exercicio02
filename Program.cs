ea89561ffb2246a1582b5477d98998ebb20d737e
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-04-04 21:14:08 -0300

    exercicio02

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula02Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre quatro números quaisquer.
+            int numero1 = 10;
+            int numero2 = 20;
+            int numero3 = 30;
+            int numero4 = 50;
+
+            double media = ((double)numero1 + (double)numero2 + (double)numero3 + (double)numero4) / 4;
+            Console.WriteLine("A média aritmética dos 4 números é: " + media);
+        }
+    }
+}
